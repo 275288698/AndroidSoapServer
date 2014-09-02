f1b7fcdf889735cb7b97214d4fda55f4e0969c65
Jan Tarnowski <tarnowski.john@gmail.com>
2014-09-02 23:38:36 +0200

    Plik nagłówkowy z którego wygenerowano stub'y.

---
+++ gsoap_header/deviceServices.h
@@ -0,0 +1,33 @@
+//gsoap ns service name:    	DeviceServices
+//gsoap ns service style:   	document
+//gsoap ns service encoding:    literal
+//gsoap ns service namespace:   http://localhost:8080/DeviceServices.wsdl
+//gsoap ns service location:    http://localhost:8080
+//gsoap ns schema namespace: 	urn:DeviceServices
+
+struct ns__Sensor
+{
+	char*   mName;
+	char*   mVendor;
+	int     mVersion;
+	int     mType;
+	float   mMaxRange;
+	float   mResolution;
+	float   mPower;
+	int     mMinDelay;
+};
+
+struct ns__SensorArray
+{
+	int __size;
+	struct ns__Sensor *sensors;
+};
+
+struct ns__Location
+{
+	double longitude;
+	double latitude;
+};
+
+int ns__getAvailableSensorsInfo(void *_, struct ns__SensorArray *sensors);
+int ns__getCurrentDeviceLocation(void *_, struct ns__Location *location);
