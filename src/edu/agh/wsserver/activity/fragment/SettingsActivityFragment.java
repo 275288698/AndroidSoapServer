e184ab6f38f0f1d1b3db159fa0305946fa3d5b74
Jan Tarnowski <tarnowski.john@gmail.com>
2015-01-03 23:05:47 +0100

    Zakladka ustawien serwera nadrzednego

--- src/edu/agh/wsserver/activity/fragment/SettingsActivityFragment.java
+++ src/edu/agh/wsserver/activity/fragment/SettingsActivityFragment.java
@@ -2,20 +2,154 @@ package edu.agh.wsserver.activity.fragment;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import edu.agh.wsserver.activity.R;
+import edu.agh.wsserver.settings.ServerSettings;
 
 public class SettingsActivityFragment extends Fragment {
-	public SettingsActivityFragment(){}
-	
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
-  
-        View rootView = inflater.inflate(R.layout.fragment_settings, container, false);
-          
-        return rootView;
-    }
+
+	private final static String LOG_TAG = SecurityActivityFragment.class.getSimpleName();
+
+	public SettingsActivityFragment() {
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		Log.d(LOG_TAG, "onCreateView START");
+
+		final View rootView = inflater.inflate(R.layout.fragment_settings, container, false);
+
+		final Button applyChanges = (Button) rootView.findViewById(R.id.buttonApplyMainServerSettings);
+		final EditText mainServerIpTxt = (EditText) rootView.findViewById(R.id.mainServerIp);
+		final EditText mainServerPortTxt = (EditText) rootView.findViewById(R.id.mainServerPort);
+
+		/* IP address */
+		mainServerIpTxt.setText(ServerSettings.getInstance().getMainServerIpAddress());
+		mainServerIpTxt.addTextChangedListener(new TextWatcher() {
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+				applyChanges.setEnabled(false);
+			}
+
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+			}
+
+			@Override
+			public void afterTextChanged(Editable s) {
+				applyChanges.setEnabled(validateFields(rootView));
+			}
+		});
+
+		/* Port number */
+		mainServerPortTxt.setText(String.valueOf(ServerSettings.getInstance().getMainServerPortNumber()));
+		mainServerPortTxt.addTextChangedListener(new TextWatcher() {
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+				applyChanges.setEnabled(false);
+			}
+
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+			}
+
+			@Override
+			public void afterTextChanged(Editable s) {
+				applyChanges.setEnabled(validateFields(rootView));
+			}
+		});
+
+		/* Apply button section */
+		applyChanges.setEnabled(false);
+		applyChanges.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				String mainServerIp = mainServerIpTxt.getText().toString();
+				if (!mainServerIp.equals(ServerSettings.getInstance().getMainServerIpAddress())) {
+					ServerSettings.getInstance().setMainServerIpAddress(mainServerIp);
+				}
+
+				Integer mainServerPortNr = Integer.parseInt(mainServerPortTxt.getText().toString());
+				if (ServerSettings.getInstance().getMainServerPortNumber() != mainServerPortNr) {
+					ServerSettings.getInstance().setMainServerPortNumber(mainServerPortNr);
+				}
+
+				applyChanges.setEnabled(false);
+			}
+		});
+
+		Log.d(LOG_TAG, "onCreateView END");
+		return rootView;
+	}
+
+	private boolean validateFields(View rootView) {
+		final EditText mainServerIpTxt = (EditText) rootView.findViewById(R.id.mainServerIp);
+		final EditText mainServerPortTxt = (EditText) rootView.findViewById(R.id.mainServerPort);
+
+		boolean returnFlag = true;
+
+		if (!validateIpAddress(mainServerIpTxt)) {
+			returnFlag = false;
+		}
+		if (!validateNumberField(mainServerPortTxt, ServerSettings.PORT_MIN, ServerSettings.PORT_MAX)) {
+			returnFlag = false;
+		}
+
+		return returnFlag;
+	}
+
+	private boolean validateIpAddress(EditText field) {
+		if (field.getText().toString().length() <= 0) {
+			field.setError("Field cannot be empty!");
+			return false;
+		} else {
+			String[] ipParts = field.getText().toString().split("\\.");
+			if (ipParts.length != 4 || field.getText().toString().endsWith(".")) {
+				field.setError("IP address has invalid format.");
+				return false;
+			}
+			for (String part : ipParts) {
+				try {
+					int value = Integer.parseInt(part);
+					if (value < 0 || value > 255) {
+						field.setError("IP address must contain numbers from range 0-255.");
+						return false;
+					}
+				} catch (NumberFormatException e) {
+					field.setError("IP address contains invalid characters.");
+					return false;
+				}
+			}
+		}
+		field.setError(null);
+		return true;
+	}
+
+	private boolean validateNumberField(EditText field, int minVal, int maxVal) {
+		if (field.getText().toString().length() <= 0) {
+			field.setError("Field cannot be empty!");
+		} else {
+			try {
+				Integer threadsCnt = Integer.parseInt(field.getText().toString());
+				if (threadsCnt >= minVal && threadsCnt <= maxVal) {
+					field.setError(null);
+					return true;
+				} else {
+					field.setError("Enter an integer value from " + minVal + " to " + maxVal + "!");
+				}
+			} catch (Exception e) {
+				Log.e(LOG_TAG, "Field value must be an integer!", e);
+				field.setError("Field value must be an integer!");
+			}
+		}
+		return false;
+	}
 }
--- src/edu/agh/wsserver/settings/ServerSettings.java
+++ src/edu/agh/wsserver/settings/ServerSettings.java
@@ -12,8 +12,11 @@ public class ServerSettings {
     public static final int PORT_DEF = 8080;
     public static final int PORT_MAX = 65535;
 
-    private int serverPortNumber;
-    private int numberOfThreads;
+    private volatile int serverPortNumber;
+    private volatile int numberOfThreads;
+
+    private volatile String mainServerIpAddress = "";
+    private volatile int mainServerPortNumber;
  
     private static volatile ServerSettings instance = null;
     
@@ -49,6 +52,24 @@ public class ServerSettings {
 
 	public void setNumberOfThreads(int numberOfThreads) {
 		this.numberOfThreads = numberOfThreads;
-        Log.d(LOG_TAG, "Number of threads has been changed: " + numberOfThreads);
+		Log.d(LOG_TAG, "Number of threads has been changed: " + numberOfThreads);
+	}
+
+	public String getMainServerIpAddress() {
+		return mainServerIpAddress;
+	}
+
+	public void setMainServerIpAddress(String mainServerIpAddress) {
+		this.mainServerIpAddress = mainServerIpAddress;
+		Log.d(LOG_TAG, "Main server ip address has been changed: " + mainServerIpAddress);
+	}
+
+	public int getMainServerPortNumber() {
+		return mainServerPortNumber;
+	}
+
+	public void setMainServerPortNumber(int mainServerPortNumber) {
+		this.mainServerPortNumber = mainServerPortNumber;
+		Log.d(LOG_TAG, "Main server port number has been changed: " + mainServerPortNumber);
 	}
 }
